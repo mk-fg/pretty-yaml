4d7f9ba382e1ac0abeafb00fce30fe2400412172
Mike Kazantsev <mk.fraggod@gmail.com>
2015-05-19 23:46:11 +0500

    Add MANIFEST.in to include README.rst

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except IOError: readme = ''
 setup(
 
 	name = 'pyaml',
-	version = '15.5.3',
+	version = '15.5.4',
 	author = 'Mike Kazantsev',
 	author_email = 'mk.fraggod@gmail.com',
 	license = 'WTFPL',
