c3191e7dba0f6b63cea76a2b3ec5746ae62ef659
Mike Kazantsev <mk.fraggod@gmail.com>
2016-09-10 09:47:13 +0500

    setup: version bump

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except IOError: readme = ''
 setup(
 
 	name = 'pyaml',
-	version = '15.8.2',
+	version = '16.9.0',
 	author = 'Mike Kazantsev',
 	author_email = 'mk.fraggod@gmail.com',
 	license = 'WTFPL',
