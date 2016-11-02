273981a4a6fff15e2e1319eb6b7205825fa4daa6
Mike Kazantsev <mk.fraggod@gmail.com>
2016-11-02 09:47:42 +0500

    setup: drop "OSI Approved" classifier, WTFPL is not

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except IOError: readme = ''
 setup(
 
 	name = 'pyaml',
-	version = '16.9.0',
+	version = '16.11.0',
 	author = 'Mike Kazantsev',
 	author_email = 'mk.fraggod@gmail.com',
 	license = 'WTFPL',
@@ -26,7 +26,6 @@ setup(
 	classifiers = [
 		'Development Status :: 4 - Beta',
 		'Intended Audience :: Developers',
-		'License :: OSI Approved',
 		'Programming Language :: Python',
 		'Programming Language :: Python :: 2.7',
 		'Programming Language :: Python :: 2 :: Only',
