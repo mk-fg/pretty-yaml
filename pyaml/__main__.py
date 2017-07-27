24bdf845a60a7d6c7d459f36ef98352c01f59ae0
Derek Schaller <dschaller@lyft.com>
2017-07-27 14:27:16 -0700

    allow line width to be passed in throguh cli

--- pyaml/__main__.py
+++ pyaml/__main__.py
@@ -9,13 +9,18 @@ def main(argv=None):
 		description='Process and dump prettified YAML to stdout.')
 	parser.add_argument('path', nargs='?', metavar='path',
 		help='Path to YAML to read (default: use stdin).')
+	parser.add_argument('-w', '--width', nargs='?', type=int,
+		help='Width to be used during yaml dump')
 	opts = parser.parse_args(argv or sys.argv[1:])
 
 	src = open(opts.path) if opts.path else sys.stdin
 	try: data = yaml.safe_load(src)
 	finally: src.close()
 
-	pyaml.pprint(data)
+	pyaml_kwargs = {}
+	if opts.width:
+		pyaml_kwargs['width'] = opts.width
+	pyaml.pprint(data, **pyaml_kwargs)
 
 
 if __name__ == '__main__': sys.exit(main())
